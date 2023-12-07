c019d3efd1fce87c1a81dadb603efe2c87ec2af7
benjGam <mecopilive@gmail.com>
2023-12-07 12:45:04 +0100

    feat: add 'create' route for products

--- exo/src/products/products.controller.ts
+++ exo/src/products/products.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { ProductsService } from './products.service';
+import CreateProductDto from './dto/create-product.dto';
 
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
+
+  @Post()
+  public create(@Body() createProductDto: CreateProductDto) {
+    return this.productsService.create(createProductDto);
+  }
 }
--- exo/src/products/products.service.ts
+++ exo/src/products/products.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma.service';
+import CreateProductDto from './dto/create-product.dto';
 
 @Injectable()
 export class ProductsService {
   constructor(private readonly prisma: PrismaService) {}
 
-  public async create()
+  public async create(createProductDto: CreateProductDto) {
+    return await this.prisma.products.create({
+      data: {
+        name: createProductDto.name,
+        description: createProductDto.description,
+        price: createProductDto.price,
+        quantity: createProductDto.quantity,
+      },
+    });
+  }
 }
