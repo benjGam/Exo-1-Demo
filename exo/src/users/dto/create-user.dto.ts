93620347835ceaaa14a029472a425089519c2820
benjGam <mecopilive@gmail.com>
2023-12-07 17:33:16 +0100

    feat: add decorators to validate data

--- exo/src/users/dto/create-user.dto.ts
+++ exo/src/users/dto/create-user.dto.ts
@@ -1,5 +1,15 @@
+import { IsString, Length } from 'class-validator';
+
 export default class CreateUserDto {
+  @IsString()
+  @Length(2, 20)
   public nickname: string;
+
+  @IsString()
+  @Length(7, 30)
   public username: string;
+
+  @IsString()
+  @Length(8, 140)
   public password: string;
 }
