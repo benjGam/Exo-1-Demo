7758b26e19b06c9f23c8a527f31012deba219763
benjGam <mecopilive@gmail.com>
2023-12-07 18:54:01 +0100

    feat: install and init response normalizer

--- exo/src/main.ts
+++ exo/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { init } from 'response-normalizer';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +10,7 @@ async function bootstrap() {
       transform: true,
     }),
   );
+  init(app);
   await app.listen(3000);
 }
 bootstrap();
